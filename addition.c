b4f10ccfb3d321146d66c16fb1254a889d97e9b3
Avenger-rohan <rk2233188@gmail.com>
2024-02-20 22:46:25 +0530

    Addition two number

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("enter number 1\n");
+    scanf("%d",&a);
+     printf("enter number 2\n");
+    scanf("%d",&b);
+    c=a+b;
+    printf("the sum is %d \n",c);
+    
+    return 0;
+}
