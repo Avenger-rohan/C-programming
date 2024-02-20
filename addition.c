d673831bfc71e2ffd37663c23c373a458a424e3f
urr-ashu <30763778+urr-ashu@users.noreply.github.com>
2024-02-20 23:07:03 +0530

    code refactored

--- addition.c
+++ addition.c
@@ -4,10 +4,8 @@ int main(){
     int a,b,c;
     printf("enter number 1\n");
     scanf("%d",&a);
-     printf("enter number 2\n");
+    printf("enter number 2\n");
     scanf("%d",&b);
-    c=a+b;
-    printf("the sum is %d \n",c);
-    
+    printf("the sum is %d \n",a+b);
     return 0;
 }
