ef7a778b95b4d476a804232dac237f3e0083010f
Avenger-rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-02-21 19:29:44 +0530

    Add files via upload

---
+++ condition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+ int main(){
+    int age;
+    printf("enter age");
+    scanf("%d",&age);
+    if(age>20){
+        printf("Adult");
+        }
+    else{
+        printf("Not Adult");
+        
+    }
+    return 0;
+ }
