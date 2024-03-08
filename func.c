3aaeeeb0534dec2cbb1698bde14c86c53e476861
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-03-08 22:19:37 +0530

    Add files via upload

---
+++ func.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void printHello();
+void printGoodbye();
+int main(){
+
+printHello();
+printGoodbye();
+return 0;
+
+}
+    void printHello() {
+    printf(" Hello/n");
+  }
+
+void printGoodbye(){
+    printf("Goodbye/n");
+}
