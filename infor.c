eb3ff172192f83e4d5626fe77917bbf7f0f4ff8f
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-04-16 22:54:44 +0530

    Add files via upload

---
+++ infor.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+int age;
+printf("enter age");
+scanf("%d",&age);
+printf(" age is%d",age);
+// age variable  name de diya user ne jab number diya vo save hogya memory location mai phie user ne printf se age ka address manga toh usne jo data save tha usse show kr diya 
+return 0;
+}
