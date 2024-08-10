b737db602954ae8eb61f8d255f1c50a95a17d70f
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-08-10 15:07:11 +0530

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// int add();
+
+
+int main(){
+  add(1,2);
+  return;
+}
+int add(){
+  int a=10,b=10,c;
+  // printf("enter no");
+  // scanf("%d",&a);
+  // printf("enter no");
+  // scanf("%d",&b);
+  c=a+b;
+  printf("Add of two no %d",c);
+  return 0;
+  
+  
+}
