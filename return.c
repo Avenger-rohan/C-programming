4832097c3e7c0eaa4f4b4bace17d5b33b9fc300f
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-08-23 01:09:21 +0530

    Add files via upload

---
+++ return.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// #include <stdbool.h>
+
+int print(){
+  int a,b;
+  printf("enter two number");
+  scanf("%d%d",&a,&b);
+  // int b;
+  //  printf("enter two number");
+  // scanf("%d",&b);
+  int sum=a+b;
+  return 5;
+
+  printf("Ans is =%d",sum);
+  // return true;
+}
+int main()
+{
+   
+  //  printf("Ans is =%d",print());
+     int response = print();
+     printf("%d",response);
+    return 0;
+}
