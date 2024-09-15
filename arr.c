f950f746548414ac5b7d857d6f8e08c7b9296648
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-09-15 22:34:03 +0530

    Add files via upload

--- arr.c
+++ arr.c
@@ -3,7 +3,11 @@ int main(){
     int ar[5];
     int i;
     for(i=0;i<5;i++){
-        printf("\nEnter the array element");
+        printf("\nEnter marks");
         scanf("%d",&ar[i]);
     }
+    for(i=1;i<5;i++){
+        printf("%d",ar[i]);
+    }
+    
 }
