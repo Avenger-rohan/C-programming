1a163bcd1d89f875b34e608c6f60eaec49fd05ac
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-09-18 23:18:24 +0530

    Add files via upload

---
+++ addres.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int arr[5];
+printf("enter no 0");
+scanf("%d",&arr[0]);
+printf("enter no 1");
+scanf("%d",&arr[1]);
+printf("enter no 2");
+scanf("%d",&arr[2]);
+printf("enter no 3");
+scanf("%d",&arr[3]);
+printf("enter no 4");
+scanf("%d",&arr[4]);
+
+printf(" %d %d %d %d %d",arr[0],arr[1],arr[2],arr[3],arr[4]);
+}
