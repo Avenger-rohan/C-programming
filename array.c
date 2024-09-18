1e419797b510b8300adc1f7ef2bc1604931d450d
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-09-19 00:41:12 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int n;
+
+
+    // for(i=0;i<5;i++){
+        printf(" enter no");
+        scanf("%d",&n);
+        for(int i=0;i<=5;i++){
+        if(n==arr[i]){
+            printf("yes");
+            break;
+        }else{
+            printf("no");
+            break;
+        }
+        
+
+
+        }
+
+        }
+        
+
+    // }
+
+    
+
+
+
+    
+
