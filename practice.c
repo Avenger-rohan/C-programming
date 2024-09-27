891c53f8ad9b7fe2b6f3ad40cd517ac2e8075603
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-09-27 23:16:57 +0530

    Add files via upload

---
+++ practice.c
@@ -0,0 +1,61 @@
+int main() 
+{
+   int n;
+   printf("enter no");
+   scanf("%d",&n);
+   
+       
+     if(n%2==0){
+        printf("even");
+         
+   }else{
+       
+       printf("odd");
+   
+       
+   
+   
+   if(n==0){
+       printf("zero\n");
+    //    printf("even\n");
+   }
+   if(n==1){
+       printf("one\n");
+    //    printf("odd\n");
+   }
+   if(n==2){
+       printf("two\n");
+    //    printf("even\n");
+   }
+   if(n==3){
+       printf("three\n");
+    //    printf("odd\n");
+   }
+   if(n==4){
+       printf("foue\n");
+    //    printf("even\n");
+   }
+   if(n==5){
+       printf("five\n");
+    //    printf("odd\n");
+   }
+   if(n==6){
+       printf("six\n");
+    //    printf("even\n");
+   }
+   if(n==7){
+       printf("seven\n");
+    //    printf("odd\n");
+   }
+   if(n==8){
+       printf("eight\n");
+    //    printf("even\n");
+   }
+   if(n==9){
+       printf("nine\n");
+    //    printf("odd\n");
+   }
+   }
+    return 0;
+}
+
