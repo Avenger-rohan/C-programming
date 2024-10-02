6d547bcc35039de3288b25564fffcfc6b681645a
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-10-02 22:55:24 +0530

    Add files via upload

---
+++ small no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    
+    int arr[5]={2,3,5,8,1};
+    int min=arr[0];
+    for(int i=0;i<=4;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }   
+        }
+        printf("%d",mminax);
+    }
