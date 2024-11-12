b09c7c943d0446528a57a54da732a46c76448836
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-11-12 22:20:42 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ 
+ int main(){
+    // int 2,3, n;
+    // printf("enter two no");
+    // scanf("%d",&n);
+    // if(2%==n){
+    //     printf(" no is not prime");
+    // }
+    // if(3%==n){
+    //     printf("no is not prime")
+    // }
+    for (int i=0; i<10; ++i)
+printf("%d", i&1);
+    return 0;
+ }
