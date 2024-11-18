17e1db093c49ebc799ec9380602e75cb99708171
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2024-11-18 22:14:05 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    int n,index=0;
+    printf("enter number");
+    scanf("%d",&n);
+    do{
+        printf("%d\n",index+1);
+       index=index+1;
+    }while(index<n);
+    return 0;
+}
+   
+//     int main() {
+//     int n, index = 0;
+//     printf("Enter number: ");
+//     scanf("%d", &n);
+//     do {
+//         printf("%d\n", index + 1);
+//         index = index + 1;
+//     } while (index < n);
+//     if (index == n) {
+//         printf("%d\n", index); // Print last index if it matches n
+//     }
+int main(){
+
+}
+
+
+           
