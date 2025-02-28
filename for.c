2ca2995623a8b2fccd73ef1e4bda77b05b8ed24b
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2025-02-28 22:39:35 +0530

    Add files via upload

---
+++ for.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+     int n;
+     scanf("%d",&n);
+     for(int i=1;i<=100;i++){
+        if(n==i){
+            printf("hai english alphabhet");
+        }else
+            printf("condition does ot match");
+            break;
+        
+     }
+    getch();         
+   
+}
