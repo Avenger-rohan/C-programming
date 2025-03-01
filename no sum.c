95afb2e9ef003b6fec9c1cb469e67f4f93ce4056
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2025-03-01 23:03:34 +0530

    Add files via upload

---
+++ no sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int num[5]={5,4,-1,7,8};
+     
+    int sum=0;
+     
+    for(int i=0;i<=4;i++){
+        sum = sum+num[i];
+            
+        }
+    
+    printf("%d",sum);
+          
+   
+}
